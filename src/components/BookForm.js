16fe056128dd782d740ad52e8baa7cd0471757bb
Raed Salah <dkraed@gmail.com>
2020-02-14 19:06:16 +0800

    created a reduce to hold all state logic

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,13 +2,19 @@ import React, { useContext, useState } from "react";
 import { BookContext } from "../contexts/BookContext";
 
 function BookForm() {
-  const { addBooks } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
-    addBooks(title, author);
+    dispatch({
+      type: "ADD_BOOK",
+      book: {
+        title,
+        author
+      }
+    });
     setTitle("");
     setAuthor("");
   };
